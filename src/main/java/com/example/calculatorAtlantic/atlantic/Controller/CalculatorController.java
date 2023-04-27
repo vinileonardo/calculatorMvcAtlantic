46bab8583a49038e6f14ff9e557b29ee11035f06
Leonardo Lima <llima@cntlog.com.br>
2023-04-26 21:02:55 -0300

    adicionado comentarios

--- src/main/java/com/example/calculatorAtlantic/atlantic/Controller/CalculatorController.java
+++ src/main/java/com/example/calculatorAtlantic/atlantic/Controller/CalculatorController.java
@@ -11,23 +11,31 @@ import java.util.Map;
 @Controller
 public class CalculatorController {
 
+    // Método que recebe uma solicitação GET na raiz do servidor e retorna o nome do arquivo HTML a ser exibido
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
+    // Método que recebe uma solicitação POST na raiz do servidor e executa o cálculo solicitado pelo usuário
     @PostMapping("/")
-    public String calculate(@RequestParam("num1") int num1,
-                            @RequestParam("num2") int num2,
-                            @RequestParam("operation") String operation,
-                            Model model) {
-        int result;
+    public String calculate(@RequestParam("num1") int num1, // Parâmetro que recebe o primeiro número
+                            @RequestParam("num2") int num2, // Parâmetro que recebe o segundo número
+                            @RequestParam("operation") String operation, // Parâmetro que recebe a operação a ser executada
+                            Model model) { // Objeto Model que será usado para adicionar atributos à resposta
+        int result; // Variável que armazenará o resultado do cálculo
+
+        // Verifica qual operação deve ser executada e realiza o cálculo correspondente
         if(operation.equals("addition")) {
-            result = num1 + num2;
+            result = num1 + num2; // Soma os dois números
         } else {
-            result = num1 - num2;
+            result = num1 - num2; // Subtrai o segundo número do primeiro número
         }
+
+        // Adiciona o resultado do cálculo ao objeto Model
         model.addAttribute("result", result);
+
+        // Retorna o nome do arquivo HTML a ser exibido (neste caso, o mesmo arquivo "index" usado no método index())
         return "index";
     }
 }
